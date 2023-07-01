7838780d85afbb030c45e5305ca9959efe6e65f9
toki0411 <toki402@naver.com>
2023-07-02 02:29:50 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/jpa/reservation/ClassReservationApplication.java
@@ -0,0 +1,13 @@
+package jpa.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassReservationApplication.class, args);
+	}
+
+}
