657c081ee0ace82cbf7ddbdd4a5e21dcaa982be2
toki0411 <toki402@naver.com>
2023-07-05 18:18:14 +0900

    feat: 검색 기능 추가

--- src/main/java/jpa/reservation/repository/ReservationRepository.java
+++ src/main/java/jpa/reservation/repository/ReservationRepository.java
@@ -1,11 +1,14 @@
 package jpa.reservation.repository;
 
-import jpa.reservation.entity.Reservation;
-import jpa.reservation.entity.ReservationStatus;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jpa.reservation.entity.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -22,6 +25,35 @@ public class ReservationRepository {
     }
 
     public void cancel(Reservation reservation) {
-        reservation.setStatus(ReservationStatus.CANCEL);
+        reservation.setStatus(ReservationStatus.CANCELED);
     }
+
+    public List<Reservation> findAll(ReservationSearch reservationSearch) {
+        QReservation reservation = QReservation.reservation;
+        QMember member = QMember.member;
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        return query.select(reservation)
+                .from(reservation)
+                .join(reservation.member, member)
+                .where(statusEq(reservationSearch.getReservationStatus()),
+                        nameLike(reservationSearch.getMemberName()))
+                .limit(1000)
+                .fetch();
+    }
+
+    private BooleanExpression statusEq(ReservationStatus statusCond) {
+        if (statusCond == null) {
+            return null;
+        }
+        return QReservation.reservation.status.eq(statusCond);
+    }
+
+    private BooleanExpression nameLike(String nameCond) {
+        if (!StringUtils.hasText(nameCond)) {
+            return null;
+        }
+        return QMember.member.name.like(nameCond);
+    }
+
 }
